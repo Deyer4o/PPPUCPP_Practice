dae97c7b6bac86b9bc7ea058a048be8a76fa502e
Deyan Mohamed <191657@buas.nl>
2025-06-03 02:31:00 +0200

    progress
    until page 34/35

--- PPPUCPP/AdditionalTools.h
+++ PPPUCPP/AdditionalTools.h
@@ -4,16 +4,29 @@
 #pragma once // Makes sure this is included only once
 import std; // Gain access to the C++ standard library (Importing the iostream module for input and output operations)
 
-#define TRACE_FUNCTION FunctionTracer tracer(__func__) // Macro to simplify usage of FunctionTracer
+const char* _curNamespace = "";
+
+#ifdef ENABLE_TRACE // If TRACE is defined, include the FunctionTracer class
+#define TRACE_FUNCTION FunctionTracer tracer(__func__, _curNamespace) // Macro to simplify usage of FunctionTracer
+#define TRACE_NAMESPACE(ns) const char* _curNamespace = ns; // Set the current namespace to Chapter1
+#else // If TRACE is not defined, do nothing
+#define TRACE_FUNCTION ; // Empty macro for FunctionTracer
+#define TRACE_NAMESPACE(ns) // Empty macro for namespace
+#endif // End of TRACE definition
 
 class FunctionTracer { // creating a FunctionTracer class
 public: //public access specifier that allows the members of the class to be accessed from outside the class
-	FunctionTracer(const char* funcName) : name(funcName) { // Constructor - inluding a funcName varriable for passing the function name
-		std::cout << "Entering: " << name << "()\n" << std::endl; // Prints function name when object gets created
+	FunctionTracer(const char* funcName, const char* aNamespaceName) : name(funcName), namespaceName(aNamespaceName){ // Constructor - inluding a funcName varriable for passing the function name
+		std::cout << "Entering: \t" << namespaceName;  // Prints function name when object gets created
+		if (namespaceName != "") std::cout << "::";
+		std::cout << name << "()\n" << std::endl;
 	}
 	~FunctionTracer() { // Destructor
-		std::cout << "\nExiting: " << name << "()" << std::endl; // Prints function name when object gets destroyed
+		std::cout << "\nExiting: \t" << namespaceName; // Prints function name when object gets destroyed
+		if (namespaceName != "") std::cout << "::";
+		std::cout << name << "()\n" << std::endl;
 	}
 private: // private access specifier that allows the members of the class to be accessed only from within the class
 	const char* name; // Declaring the variable "name"
+	const char* namespaceName; // Declaring the variable "namespace"
 };
